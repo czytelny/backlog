5ceb01d3e9967cde08c7f1db215df9e3ef7e8355
Michal Chwedczuk <czytelny@gmail.com>
2019-03-15 19:17:16 +0100

    margin increased

--- src/renderer/components/BoardContent.vue
+++ src/renderer/components/BoardContent.vue
@@ -148,7 +148,7 @@
   .board-top-actions {
     /*background-color: #fff;*/
     background-image: linear-gradient(to bottom, #fff, #fff);
-    padding: 24px 16px 8px 16px;
+    padding: 32px 16px 8px 16px;
     width: 100%;
     margin-top: -16px;
     box-shadow: 0 0 8px #e2e2e2;
--- src/renderer/components/BoardsMenu.vue
+++ src/renderer/components/BoardsMenu.vue
@@ -102,7 +102,7 @@
 
   .board-menu {
     width: 100%;
-    padding: 24px;
+    padding: 32px;
     transition: all .3s;
     box-shadow: 0 0 16px rgba(57, 59, 62, 0.11);
   }
