ebb8c41e93e6f3d5b34d0b2d583bf18cd71dd8ee
Michal Chwedczuk <czytelny@gmail.com>
2019-08-17 20:34:07 +0200

    fallback local set to en

--- src/main.js
+++ src/main.js
@@ -3,22 +3,11 @@ import VueRouter from 'vue-router';
 import store from './store';
 import App from './App';
 import VueShortkey from 'vue-shortkey';
-import VueI18n from 'vue-i18n'
-import en from './i18n/en'
-import zh from './i18n/zh'
-import pl from './i18n/pl'
-import ptBR from './i18n/pt-BR'
-
-const messages = {
-  en: en,
-  zh: zh,
-  pl: pl,
-  ptBR: ptBR
-};
-
-Vue.use(VueI18n);
-const i18n = new VueI18n({ locale: 'en', messages });
-
+import VueI18n from 'vue-i18n';
+import en from './i18n/en';
+import zh from './i18n/zh';
+import pl from './i18n/pl';
+import ptBR from './i18n/pt-BR';
 import {
   Button,
   Checkbox,
@@ -30,24 +19,38 @@ import {
   Message,
   Modal,
   Progress,
-  Tooltip
+  Tooltip,
 } from 'iview';
 import BoardContent from './components/board/BoardContent.vue';
 import VueTextareaAutosize from 'vue-textarea-autosize';
 
+const messages = {
+  en: en,
+  zh: zh,
+  pl: pl,
+  ptBR: ptBR,
+};
+
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+  locale: 'en',
+  messages,
+  fallbackLocale: 'en',
+});
+
 require('autolink-js');
 
 Vue.config.productionTip = false;
-Vue.config.devtools =  process.env.NODE_ENV !== 'production';
+Vue.config.devtools = process.env.NODE_ENV !== 'production';
 
 const EventBus = new Vue();
 
 Object.defineProperties(Vue.prototype, {
   $bus: {
-    get: function () {
+    get: function() {
       return EventBus;
-    }
-  }
+    },
+  },
 });
 Vue.use(VueShortkey);
 Vue.use(VueTextareaAutosize);
@@ -66,25 +69,25 @@ Vue.prototype.$Message = Message;
 Vue.prototype.$Modal = Modal;
 
 Vue.directive('focus', {
-  componentUpdated: function (el) {
+  componentUpdated: function(el) {
     el.getElementsByTagName('input')[0].focus();
-  }
+  },
 });
 
 Vue.filter('metaTextReplacer', text => text.replace('META', '⌘'));
 Vue.filter('shiftTextReplacer', text => text.replace('SHIFT', '⇧'));
 
 const routes = [
-  {path: '/board/:boardId/:itemId?', component: BoardContent}
+  {path: '/board/:boardId/:itemId?', component: BoardContent},
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 new Vue({
   i18n,
   store,
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app');
